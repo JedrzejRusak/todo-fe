72c6548ccef5c88d0d14022f3aad2135e169c74f
Jedrzej Rusak <j.rusak67@gmail.com>
2023-06-08 12:05:38 +0200

    fronten intit

---
+++ src/Entry.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+const Entry = () => {
+    return (
+        <>
+            <BrowserRouter>
+                { }
+                <Routes>
+
+                </Routes>
+                { }
+            </BrowserRouter>
+        </>
+    );
+};
+
+export default Entry;
